cd70f97a4922a8398cc7119cbc53845248950b93
Rakib Islam <rakibislam.rh330@gmail.com>
2026-04-16 12:40:47 +0600

    New try code

---
+++ basc_python/python_basic_input_out.py
@@ -0,0 +1,16 @@
+name = input("Enter your Name: ")
+print(name)
+
+F_money = input("enter first amount: " )
+S_money = input("Enter secend amount: ")
+
+Total_amount = F_money + S_money
+
+print(Total_amount)
+
+F_money = input("enter first amount: " )
+S_money = input("Enter secend amount: ")
+
+Total_amount = int(F_money) + int(S_money)
+
+print(Total_amount)
---
+++ basc_python/python_va.py
@@ -0,0 +1,21 @@
+print("Hello world")
+
+age = 12
+name = "Rakib"
+is_single = True
+interest = 12.50
+
+
+print(name, type(name))
+print(age, type(age))
+print(is_single, type(is_single))
+print(interest, type(interest))
+
+
+# F string try
+
+cname = 'Tushar'
+age = 24
+print(f"Hello, This is {cname} and I'm {age} years old.")
+
+
