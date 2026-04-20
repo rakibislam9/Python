9fd8b096cb487edbb7e8eff1ac64ecc3f7633296
Rakib Islam <rakibislam.rh330@gmail.com>
2026-04-20 11:43:00 +0600

    for loop

---
+++ basc_python/for_loop.py
@@ -0,0 +1,11 @@
+# Example: range with only stop argument
+
+for i in range(5):
+    print(i)
+
+
+for num in range(1, 10):
+    if num % 2 == 0:
+        print (num , "is even")
+    else:
+        print(num, "is odd")
