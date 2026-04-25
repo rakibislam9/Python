407ab8e4ed21bc6d6345f86e7abe8e4e3df3777e
Rakib Islam <rakibislam.rh330@gmail.com>
2026-04-25 20:54:30 +0600

    Builtinfunction

---
+++ Modile-2/Builtinfunction.py
@@ -0,0 +1,19 @@
+# highest = max([3,5,6,9,8])
+# print(highest)
+
+
+# highest = min([3,5,6,9,8])
+# print(highest)
+
+
+#  import functions
+
+def sum(a,b):
+    return a+b
+
+# modules.py
+
+# from functions import sum
+
+result = sum(5,6)
+print(result)
