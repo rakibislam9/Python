170a2d47713898bb86bbb0b1697dad4d09d936e9
Rakib Islam <rakibislam.rh330@gmail.com>
2026-04-28 13:02:58 +0600

    String

---
+++ modiule-3/string.py
@@ -0,0 +1,13 @@
+# mystring = "i am a Rakib islam"
+
+# capitalized_string = mystring.capitalize()
+# print(capitalized_string)
+# print(mystring)
+
+
+# returns the centered padded string of length 24
+
+sentence = "python is awesome"
+
+new_string = sentence.center(24, '*')
+print(new_string)
